9f801132cff56a4d0951fa9e1ada9c349648e002
Grégoire Cutzach <caztuc@hotmail.com>
2018-10-16 13:19:19 +0200

    ADDED socket connection

---
+++ socket_server/app.js
@@ -0,0 +1,26 @@
+console.log("hello world");
+
+const WebSocket = require('ws');
+
+const wss = new WebSocket.Server({ port: 8080 });
+
+function noop() {}
+
+function heartbeat() {
+  this.isAlive = true;
+}
+
+wss.on('connection', function connection(ws) {
+  ws.isAlive = true;
+  ws.on('pong', heartbeat);
+  console.log("some is connecting", ws);
+});
+
+const interval = setInterval(function ping() {
+  wss.clients.forEach(function each(ws) {
+    if (ws.isAlive === false) return ws.terminate();
+
+    ws.isAlive = false;
+    ws.ping(noop);
+  });
+}, 30000);
